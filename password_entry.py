dbb7a274bf9e62090e5819f9cff994570f054c11
jc320054 <lukas.arnold@my.jcu.edu.au>
2019-03-21 13:48:35 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,17 @@
 """"
 Lukas Arnold
 """
+
+"""
+Warm up question password check
+"""
+
+password = str(input("Enter a password for checking: "))
+MIN_NO_CHARACTERS = 6
+
+if len(password) >= MIN_NO_CHARACTERS:
+    print("Your password meets the minimum length requirement!")
+    print("*" * len(password))
+else:
+    print("Your password does not meet the minimum length requirement! Restart program.")
+
